4612333424e673c04b2ec3b0eab872c1036ca548
alsun <aminabdo8@gmail.com>
2017-07-27 05:46:26 +0200

    finish fourth section 'products and features' by html,css and javascript

--- js/script.js
+++ js/script.js
@@ -92,6 +92,131 @@ $(document).ready(function () {
         });
     });
 
+
+    $('.items .item-product-5').hover(function () {
+        $('.items .item-product-5 .item-hover').show();
+        $('.items .item-product-5 .item-text hr').css({
+            top: 55 + '%'
+        });
+        $('.items .item-product-6 .item-text hr').css({
+            top: 75 + '%'
+        });
+        $('.items .item-product-7 .item-text hr').css({
+            top: 75 + '%'
+        });
+        $('.items .item-product-8 .item-text hr').css({
+            top: 75 + '%'
+        });
+    }, function () {
+        $('.items .item-product-5 .item-hover').hide();
+        $('.items .item-product-5 .item-text hr').css({
+            top: 69 + '%'
+        });
+        $('.items .item-product-6 .item-text hr').css({
+            top: 69 + '%'
+        });
+        $('.items .item-product-7 .item-text hr').css({
+            top: 69 + '%'
+        });
+        $('.items .item-product-8 .item-text hr').css({
+            top: 69 + '%'
+        });
+
+    });
+
+    $('.items .item-product-6').hover(function () {
+        $('.items .item-product-6 .item-hover').show();
+        $('.items .item-product-6 .item-text hr').css({
+            top: 55 + '%'
+        });
+        $('.items .item-product-5 .item-text hr').css({
+            top: 75 + '%'
+        });
+        $('.items .item-product-7 .item-text hr').css({
+            top: 75 + '%'
+        });
+        $('.items .item-product-8 .item-text hr').css({
+            top: 75 + '%'
+        });
+    }, function () {
+        $('.items .item-product-6 .item-hover').hide();
+        $('.items .item-product-6 .item-text hr').css({
+            top: 69 + '%'
+        });
+        $('.items .item-product-5 .item-text hr').css({
+            top: 69 + '%'
+        });
+        $('.items .item-product-7 .item-text hr').css({
+            top: 69 + '%'
+        });
+        $('.items .item-product-8 .item-text hr').css({
+            top: 69 + '%'
+        });
+
+    });
+
+    $('.items .item-product-7').hover(function () {
+        $('.items .item-product-7 .item-hover').show();
+        $('.items .item-product-7 .item-text hr').css({
+            top: 55 + '%'
+        });
+        $('.items .item-product-5 .item-text hr').css({
+            top: 75 + '%'
+        });
+        $('.items .item-product-6 .item-text hr').css({
+            top: 75 + '%'
+        });
+        $('.items .item-product-8 .item-text hr').css({
+            top: 75 + '%'
+        });
+    }, function () {
+        $('.items .item-product-7 .item-hover').hide();
+        $('.items .item-product-7 .item-text hr').css({
+            top: 69 + '%'
+        });
+        $('.items .item-product-5 .item-text hr').css({
+            top: 69 + '%'
+        });
+        $('.items .item-product-6 .item-text hr').css({
+            top: 69 + '%'
+        });
+        $('.items .item-product-8 .item-text hr').css({
+            top: 69 + '%'
+        });
+
+    });
+
+    $('.items .item-product-8').hover(function () {
+        $('.items .item-product-8 .item-hover').show();
+        $('.items .item-product-8 .item-text hr').css({
+            top: 55 + '%'
+        });
+        $('.items .item-product-5 .item-text hr').css({
+            top: 75 + '%'
+        });
+        $('.items .item-product-6 .item-text hr').css({
+            top: 75 + '%'
+        });
+        $('.items .item-product-7 .item-text hr').css({
+            top: 75 + '%'
+        });
+    }, function () {
+        $('.items .item-product-8 .item-hover').hide();
+        $('.items .item-product-8 .item-text hr').css({
+            top: 69 + '%'
+        });
+        $('.items .item-product-5 .item-text hr').css({
+            top: 69 + '%'
+        });
+        $('.items .item-product-6 .item-text hr').css({
+            top: 69 + '%'
+        });
+        $('.items .item-product-7 .item-text hr').css({
+            top: 69 + '%'
+        });
+
+    });
+
     $('.nav-toggle').click(function () {
         $('.nav-ul').toggleClass('active');
     });
